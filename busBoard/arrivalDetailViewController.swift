518d5132d7b0f48967358f3ae12e3536259b11df
Blydro Klonk <blydro@gmail.com>
2018-08-09 00:57:03 +0200

    Fix regression from 0e0756fe9327b96872ef85f9a4ebcce9d801fe61

--- busBoard/arrivalDetailViewController.swift
+++ busBoard/arrivalDetailViewController.swift
@@ -64,18 +64,22 @@ class arrivalDetailViewController: UIViewController {
         //It is safe to only incrmenet this by one and then not check again because you know what if it's the same a third time than maybe this bus really is showing up that often
         //sometimes it's not the software's fault it's the transit company's fault
         
-        //TODO: maybe it is my fault: check for cacnellation here?
+        //TODO: while loop you fucking idiot
         var arrivalCounter = 0
         
         when1Label.text = getDiffString(arrivalTime: nextArrivals[arrivalCounter].arrivalTime)
         if(nextArrivals.count > 1) {
+            arrivalCounter += 1
+            
             if(nextArrivals[arrivalCounter].arrivalTime == nextArrivals[arrivalCounter + 1].arrivalTime) {
-                arrivalCounter += 1
+                arrivalCounter += 1 //push it even further if it's the same
             }
             when2Label.text = getDiffString(arrivalTime: nextArrivals[arrivalCounter].arrivalTime)
             if(nextArrivals.count > 2) {
+                arrivalCounter += 1
+                
                 if(nextArrivals[arrivalCounter].arrivalTime == nextArrivals[arrivalCounter + 1].arrivalTime) {
-                    arrivalCounter += 1
+                    arrivalCounter += 1 //push it even further if it's the same
                 }
                 when3Label.text = getDiffString(arrivalTime: nextArrivals[2].arrivalTime)
             }
